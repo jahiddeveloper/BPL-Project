64a5efe0fccc382bae568ad15f98bcd7f031aa95
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2026-01-04 16:55:54 +0600

    Navbar compleat

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,9 +1,72 @@
-import React from 'react';
+import React from "react";
+import navImg from "../../assets/logo.png";
+import coin from "../../assets/coin-1.png"
 
 const Navbar = () => {
-    return (
-        
-    );
+  return (
+    <div className="bg-base-100 shadow-sm">
+      <div className="navbar container mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>Fixture</a>
+              </li>
+              <li>
+                <a>Teams</a>
+              </li>
+              <li>
+                <a>Schedules</a>
+              </li>
+            </ul>
+          </div>
+          <img src={navImg} alt="" />
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 md:mx-3">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>Fixture</a>
+            </li>
+            <li>
+              <a>Teams</a>
+            </li>
+            <li>
+              <a>Schedules</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="font-bold btn"><span>600000000</span>Coin <img src={coin} alt="" /></a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
